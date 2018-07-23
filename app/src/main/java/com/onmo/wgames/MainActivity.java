fe6a42748fb66b0be65bc3245d1853ab410a2904
srini <srinivasvadde@gmail.com>
2018-07-24 00:01:30 +0530

    updated sample app for testing

--- app/src/main/java/com/onmo/wgames/MainActivity.java
+++ app/src/main/java/com/onmo/wgames/MainActivity.java
@@ -1,11 +1,8 @@
 package com.onmo.wgames;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.onmo.wgames.sdk.IResponseHandler;
@@ -16,66 +13,68 @@ import com.onmo.wgames.sdk.SDKException;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private TextView mTextMessage;
+    private TextView tv_method_1;
+    private TextView tv_method_2;
+    private TextView response_message;
+    private IWGameSession mWGSession ;
 
-    IWGameSession mWGSession ;
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        tv_method_1 = findViewById(R.id.tv_method_1);
+        tv_method_2 = findViewById(R.id.tv_method_2);
 
-
+        response_message = findViewById(R.id.response_message);
         // getting session object to access the SDK methods
         mWGSession = OnmoWGSDK.newInitializer(MainActivity.this)
                 .build();
 
-    }
+        tv_method_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
 
+                if(mWGSession!=null)
+                {
+                    mWGSession.getStoreUser(new IResponseHandler<String>() {
+                        @Override
+                        public void handleResponse(String aUserId) {
+                            response_message.setText("User id:"+aUserId);
+                        }
 
-    private void getUser()
-    {
-        Log.d(TAG,"getUser-->");
+                        @Override
+                        public void handleException(SDKException exception) {
+                            response_message.setText("error::"+exception.getMessage());
+                        }
+                    });
+                }
+            }
+        });
 
-        if(mWGSession!=null)
-        {
-            mWGSession.getStoreUser(new IResponseHandler<String>() {
-                @Override
-                public void handleResponse(String result) {
+        tv_method_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+
+                if(mWGSession!=null)
+                {
+                    mWGSession.getConfig(new IResponseHandler<String>() {
+                        @Override
+                        public void handleResponse(String config) {
+                            response_message.setText("getConfig:"+config);
+                        }
+
+                        @Override
+                        public void handleException(SDKException exception) {
+                            response_message.setText("error::"+exception.getMessage());
+                        }
+                    });
                 }
 
-                @Override
-                public void handleException(SDKException exception) {
-
-                }
-            });
-        }
-
-
-
+            }
+        });
     }
+
 }
